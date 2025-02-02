e6b258688a9d9c47113bbb5b0efa41af8d64a5ec
devhrvx <salinas.johnharvey11@gmail.com>
2025-02-02 11:51:54 +0800

    Add initial HTML, CSS, and JavaScript files for the openloveboard project

---
+++ script.js
@@ -0,0 +1,12 @@
+$(window).on('load', function () {
+    $('.loader').fadeOut();
+    $('.content').fadeIn();
+    $('.content').addClass('visible');
+});
+
+var recipientName = "John Harvey Salinas";
+var strandSection = "ICT402";
+var date = new Date();
+var senderName = "Mr. John Doe";
+var messageContent = "Hello, I really like your smell and hair";
+
